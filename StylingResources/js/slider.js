df6ae4a556264d8014e86c2eac7293ed07d821a5
Justine Pham <stinamanalily@gmail.com>
2015-10-12 15:15:31 -0500

    added some changes and files

--- StylingResources/js/slider.js
+++ StylingResources/js/slider.js
@@ -1,36 +1,25 @@
 var main = function() {
    
-
+var Visible = $('.active-slide');
    console.log( $('.active-slide').attr("class") )
    $('.opts').click(function() { 
-	    var Visible = $('.active-slide');
-
+	    
 		var getSlide = $(this).attr("title");
 		getSlide = '.' + getSlide;
 		
 		var thisSlide = $(getSlide);
-		console.log('Current: ' + Visible.attr("class"))
-		console.log('New: ' + thisSlide.attr("class"))
-
+		//console.log('Current: ' + Visible.attr("class"))
+		//console.log('New: ' + thisSlide.attr("class"))
 		// REMOVE THIS SLIDE 
 		Visible.fadeOut(1000).removeClass('active-slide');
-
-
 		//Visible.addClass('hidden');
-		console.log('Current: ' + Visible.attr("class"))
-		console.log('New: ' + thisSlide.attr("class"))
-		
+		//console.log('Current: ' + Visible.attr("class"))
+		//console.log('New: ' + thisSlide.attr("class"))	
 		// ADD THIS SLIDE
 		thisSlide.fadeIn(1000).addClass('active-slide');
-
-		//alert( thisSlide.attr("class"));
-		//var SlideClass = thisSlide.attr("class") + ' active-slide';
-		//thisSlide.attr("class", SlideClass);
-		//thisSlide.removeClass('hidden');
-
-		console.log('Current: ' + Visible.attr("class"))
-		console.log('New: ' + thisSlide.attr("class"))
-		
+		//console.log('Current: ' + Visible.attr("class"))
+		//console.log('New: ' + thisSlide.attr("class"))
+		Visible = thisSlide;
 		
   });
   
