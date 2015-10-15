6efc21eca72dd37c1b6c88f6fc02adf1e6b86685
Justine Pham <stinamanalily@gmail.com>
2015-10-15 13:38:41 -0500

    add some angular js

---
+++ StylingResources/js/routing.js
@@ -0,0 +1,13 @@
+var app=angular.module('single-page-app',['ngRoute']);
+app.config(function($routeProvider){
+      $routeProvider
+          .when('/home',{
+                templateUrl: 'home.html'
+          })
+          
+		  .when('/portfolio',{
+                templateUrl: 'portfolio.html'
+          });
+
+});
+
